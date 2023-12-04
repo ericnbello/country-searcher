e286f172ba5cea1fc17a3ab804f6d3e901edc197
Eric Bello <ebello219@gmail.com>
2023-12-04 13:56:27 -0500

    resolved jsx-a11y error

--- src/components/ThemeToggle.js
+++ src/components/ThemeToggle.js
@@ -4,7 +4,7 @@ export default function ThemeToggle(props) {
     return (
         <div>
             <button onClick={props.toggleDarkMode} className="flex gap-2">
-                <img className="w-6 h-6" src={props.darkMode ? "./sun-solid.svg" : "./moon-solid.svg"}  
+                <img className="w-6 h-6" src={props.darkMode ? "./sun-solid.svg" : "./moon-solid.svg"} alt="dark mode theme toggle icon"
                 />  
                 <p>{props.darkMode ? "Light Mode" : "Dark Mode"}</p>
             </button>        
